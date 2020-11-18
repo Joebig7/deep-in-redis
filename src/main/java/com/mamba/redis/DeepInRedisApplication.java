0317b4f2a10fd14a80023f7e0e57ca1ab7b5cea7
joebig7 <1479762535@qq.com>
2020-11-18 11:37:42 +0800

    add README.md

---
+++ src/main/java/com/mamba/redis/DeepInRedisApplication.java
@@ -0,0 +1,13 @@
+package com.mamba.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepInRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepInRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mamba/redis/DeepInRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamba.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepInRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
